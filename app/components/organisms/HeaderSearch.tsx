685930ca9da33f04f6b969ca2ec65b1409f9095f
Valentin Menvielle Candia <menvielle.valen@gmail.com>
2024-08-06 01:31:17 -0300

    fix suspense

--- app/components/organisms/HeaderSearch.tsx
+++ app/components/organisms/HeaderSearch.tsx
@@ -1,11 +1,14 @@
 "use client"
 
+import { Suspense } from "react"
 import { Searchbar } from "../atoms/Searchbar"
 
 export const HeaderSearch = () => {
   return (
     <header className="w-full fixed p-4 z-[1] bg-gray-200 bg-opacity-70 shadow-sm">
-        <Searchbar/>
+        <Suspense>
+          <Searchbar/>
+        </Suspense>
     </header>
   )
 }
